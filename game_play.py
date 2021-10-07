cac9029abe70858c97856516c82255ba763f2ddf
unknown <dane.johnson451@gmail.com>
2021-10-07 15:28:14 -0700

    Added Load/Save Functionality

--- game_play.py
+++ game_play.py
@@ -1,3 +1,8 @@
+# Verify keyboard import
+# Verify keycheck + save + load json files
+
+import keyboard
+import jsonpickle
 from os import system
 import time
 
@@ -5,11 +10,16 @@ from grid import Grid
 from ai_player import AIPlayer
 from player import Player
 
-class Gameplay:
+class Gameplay(object):
 
     def __init__(self, player):
         self.board = Grid()
         self.player = player
+        self.saved_coords_list = []
+
+    def reload_coords(self):
+        for coords in self.saved_coords_list:
+            self.board.value_generator(coords[0], coords[1])
 
     def solved(self):
         return self.board.total_mines_calculator() == self.board.total_mines_revealed_calculator()
@@ -27,10 +37,11 @@ class Gameplay:
             system("cls")
             print("BLAST!!!!")
             time.sleep(2)
-            system("cls")
-            self.board.display_grid()
         else:
             self.board.value_generator(x, y)
+            self.saved_coords_list.append([x, y])
+            self.execute_flag_choice(self.player.flag_choice())
+            time.sleep(1)
 
     def execute_flag_choice(self, choice):
         if self.validate_flag_choice(choice):
@@ -47,8 +58,34 @@ class Gameplay:
             result = self.player.make_guess()
             if self.validate_input_value(result):
                 self.execute_guess(int(result[0]), int(result[2]))
-                self.execute_flag_choice(self.player.flag_choice())
                 time.sleep(1)
 
-mine = Gameplay(AIPlayer())
-mine.play()
+def load_game():
+    with open("minesweeper.json", "r") as file:
+        try:
+            my_file = file.read()
+            return jsonpickle.decode(my_file)
+        except:
+            return Gameplay(Player())
+
+def save_game(game):
+    with open("minesweeper.json", "w") as file:
+        my_file = jsonpickle.encode(game)
+        file.write(my_file)
+
+def clear_game():
+    with open("minesweeper.json", "w") as file:
+        file.truncate(0)
+
+
+mine = load_game()
+if(len(mine.saved_coords_list) == 0):
+    pass
+else:
+    mine.reload_coords()
+try:
+    mine.play()
+    clear_game()
+except KeyboardInterrupt:
+    save_game(mine)
+    exit()
